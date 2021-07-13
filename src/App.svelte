87d6c3832f1d4490d7fc4fcbda997ac595195d22
� Simone <simonetedesco.dev@gmail.com>
2021-07-13 23:59:05 +0200

    added header

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script>
-	export let name;
+  import Header from './components/Header';
 </script>
 
+<Header />
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione obcaecati placeat et deleniti provident eveniet nisi. Quia quidem velit inventore magni, quisquam quis aspernatur. Doloremque beatae atque vero sint repellendus.</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main{
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+<header>
+  <h1>
+    <img src="/img/bg.jpg" alt="alt txt" />
+  </h1>
+</header>
+<style></style>
