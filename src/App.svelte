85ba7414ca32fb9f3a794ff6d665ebc6afeb7ea1
� Simone <simonetedesco.dev@gmail.com>
2021-07-19 23:57:35 +0200

    added tabs component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script>
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
+  import Tabs from './shared/Tabs.svelte';
+
+  // tabs
+  let items = ["current polls", "add new poll"];
+  let activeItem = "current polls";
 </script>
 
 <Header />
 <main>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione obcaecati placeat et deleniti provident eveniet nisi. Quia quidem velit inventore magni, quisquam quis aspernatur. Doloremque beatae atque vero sint repellendus.</p>
+  <Tabs {items} {activeItem} />
 </main>
 <Footer />
 
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let items;
+  export let activeItem;
+</script>
+
+<div class="tabs">
+  <ul>
+    <!-- {#each items as item}
+      
+    {/each} -->
+  </ul>
+</div>
+
+<style></style>
