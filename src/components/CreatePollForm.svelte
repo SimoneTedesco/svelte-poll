65f16a98ba997bda5e7c2e4a323d7a0f7bf5cbad
� Simone <simonetedesco.dev@gmail.com>
2021-07-26 23:51:04 +0200

    added form validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,8 +1,33 @@
 <script>
   import Button from '../shared/Button.svelte';
-  let fields = {question: '', answerA: '', answerB: ''}
+  let fields = {question: '', answerA: '', answerB: ''};
+  let errors = {question: '', answerA: '', answerB: ''};
+  let valid = false;
   const submitHandler = () => {
-    console.log('fields :>> ', fields);
+    valid = true;
+    
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = 'Question must be at least 5 characters';
+    } else {
+      errors.question = '';
+    }
+    if (fields.answerA.trim().length  < 1) {
+      valid = false;
+      errors.answerA = 'Answer A must be at most 1 character';
+    } else {
+      errors.answerA = '';
+    }
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = 'Answer B must be at most 1 character';
+    } else {
+      errors.answerB = '';
+    }
+
+    if (valid) {
+      console.log('valid :>> ', fields);
+    }
   }
 </script>
 
@@ -10,14 +35,17 @@
   <div class="form-field">
     <label for="question">question</label>
     <input type="text" id="question" bind:value={fields.question}>
+    <div class="error">{errors.question}</div>
   </div>
   <div class="form-field">
     <label for="answer-a">answer-a</label>
     <input type="text" id="answer-a" bind:value={fields.answerA}>
+    <div class="error">{errors.answerA}</div>
   </div>
   <div class="form-field">
     <label for="answer-b">answer-b</label>
     <input type="text" id="answer-b" bind:value={fields.answerB}>
+    <div class="error">{errors.answerB}</div>
   </div>
   <Button type="secondary">Add poll</Button>
 
@@ -37,6 +65,11 @@
   label {
     width: 10px auto;
     text-align: left;
+  }
+  .error {
+    color: red;
+    font-weight: bold;
+    font-size: 12px;
   }
 
 </style>
