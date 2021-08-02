9d5fd0adf15026db37ba43a36edcae9b33fe88f3
� Simone <simonetedesco.dev@gmail.com>
2021-08-02 23:49:00 +0200

    added card component

---
+++ src/shared/Card.svelte
@@ -0,0 +1,13 @@
+<div class="card">
+  <slot></slot>
+</div>
+
+<style>
+  .card {
+    background-color: #f5f5f5;
+    padding: 20px;
+    border-radius: 6px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    color: black;
+  }
+</style>
