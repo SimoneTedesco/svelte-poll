3484019db1083f31ebae0c0a51ee9790d0b8862a
� Simone <simonetedesco.dev@gmail.com>
2021-08-07 22:22:39 +0200

    handle vote function

--- src/App.svelte
+++ src/App.svelte
@@ -26,6 +26,13 @@
     activeItem = "current polls";
     console.log('polls :>> ', polls);
   }
+  
+  const handleVote = (e) => {
+    const { id, option } = e.detail;
+    const vote = option === "a" ? "votesA" : "votesB";
+    polls = polls.map(p => p.id === id ? { ...p, [vote]: p[vote] + 1 } : p);
+  }
+
 </script>
 
 <Header />
@@ -33,7 +40,7 @@
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione obcaecati placeat et deleniti provident eveniet nisi. Quia quidem velit inventore magni, quisquam quis aspernatur. Doloremque beatae atque vero sint repellendus.</p>
   <Tabs {items} {activeItem} on:tabChange={tabChange} />
   {#if activeItem === "current polls"}
-    <PollList {polls} on:vote={handleVotes} />
+    <PollList {polls} on:vote={handleVote} />
   {:else if activeItem === "add new poll"}
     <CreatePollForm on:add={handleAdd} />
   {/if}
