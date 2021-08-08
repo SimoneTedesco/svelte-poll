12f671a241c78b37d840867c3d0352eb9cdda12e
� Simone <simonetedesco.dev@gmail.com>
2021-08-08 22:36:29 +0200

    percentage bars

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -7,6 +7,8 @@
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor(poll.votesA / totalVotes * 100);
+  $: percentB = Math.floor(poll.votesB / totalVotes * 100);
 
   // handling votes
   const handleVotes = (option, id) => {
@@ -19,16 +21,29 @@
     <h3>{poll.question}</h3>
     <p>Total votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVotes("a", poll.id)}>
-      <div class="percent percent-a"></div>
+      <div class="percent percent-a" style="width: {percentA}%;"></div>
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVotes("b", poll.id)}>
-      <div class="percent percent-b"></div>
+      <div class="percent percent-b" style="width: {percentB}%;"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
   </div>
 </Card>
 
 <style>
-
+  .answer {
+    position: relative;
+  }
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+  .percent-a {
+    background-color: rgba(218, 91, 91, 0.164);
+  }
+  .percent-b {
+    background-color: rgba(152, 218, 91, 0.164);
+  }
 </style>
